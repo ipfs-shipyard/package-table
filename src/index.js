ccfcb6b076c621e9c4237ec8b18b039b3b031da6
Henrique Dias <hacdias@gmail.com>
2018-08-22 11:38:49 +0100

    feat: only print indicated columns
    License: MIT
    Signed-off-by: Henrique Dias <hacdias@gmail.com>

--- src/index.js
+++ src/index.js
@@ -14,29 +14,25 @@ const data = require(path.join(process.cwd(), argv.data))
 
 const isItemPackage = (item) => Array.isArray(item)
 
-const packageBadges = [
-  // Package
-  (gh, npm) => `[\`${npm}\`](//github.com/${gh})`,
-  // Version
-  (gh, npm) => `[![npm](https://img.shields.io/npm/v/${npm}.svg?maxAge=86400&style=flat-square)](//github.com/${gh}/releases)`,
-  // Deps
-  (gh, npm) => `[![Deps](https://david-dm.org/${gh}.svg?style=flat-square)](https://david-dm.org/${gh})`,
-  // CI
-  (gh, npm) => {
+const packageBadges = {
+  'Package': (gh, npm) => `[\`${npm}\`](//github.com/${gh})`,
+  'Version': (gh, npm) => `[![npm](https://img.shields.io/npm/v/${npm}.svg?maxAge=86400&style=flat-square)](//github.com/${gh}/releases)`,
+  'Deps': (gh, npm) => `[![Deps](https://david-dm.org/${gh}.svg?style=flat-square)](https://david-dm.org/${gh})`,
+  'CI': (gh, npm) => {
     // Need to fix the path for jenkins links, as jenkins adds `/job/` between everything
     const jenkinsPath = gh.split('/').join('/job/')
     return `[![jenkins](https://ci.ipfs.team/buildStatus/icon?job=${gh}/master)](https://ci.ipfs.team/job/${jenkinsPath}/job/master/)`
   },
-  // Coverage
-  (gh, npm) => `[![codecov](https://codecov.io/gh/${gh}/branch/master/graph/badge.svg)](https://codecov.io/gh/${gh})`
-]
+  'Coverage': (gh, npm) => `[![codecov](https://codecov.io/gh/${gh}/branch/master/graph/badge.svg)](https://codecov.io/gh/${gh})`
+}
 
 // Creates the table row for a package
 const generatePackageRow = (item) => {
-  const row = packageBadges.map((func) => {
+  const row = data.columns.map(col => {
     // First string is GitHub path, second is npm package name
-    return func(item[0], item[1])
+    return packageBadges[col](item[0], item[1])
   }).join(' | ')
+
   const fullRow = `| ${row} |`
   return fullRow
 }
