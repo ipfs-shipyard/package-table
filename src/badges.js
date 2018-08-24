3a4c27f7b537d7333412ed6733e5bbf7ccf45986
Henrique Dias <hacdias@gmail.com>
2018-08-24 15:07:34 +0100

    feat: go and travis support, ignores interface- badges (#1)

---
+++ src/badges.js
@@ -0,0 +1,67 @@
+const https = require('https')
+
+const badgeExists = url => new Promise(resolve => {
+  const req = https.request(url, {
+    method: 'HEAD'
+  }, res => {
+    resolve(res.statusCode !== 404)
+  })
+
+  req.on('error', _ => {
+    resolve(false)
+  })
+
+  req.end()
+})
+
+const name = (gh, pkg) => {
+  return `[\`${pkg}\`](//github.com/${gh})`
+}
+
+const npmVersion = (gh, pkg) => {
+  return `[![npm](https://img.shields.io/npm/v/${pkg}.svg?maxAge=86400&style=flat-square)](//github.com/${gh}/releases)`
+}
+
+const deps = ({gh}) => {
+  return `[![Deps](https://david-dm.org/${gh}.svg?style=flat-square)](https://david-dm.org/${gh})`
+}
+
+const ciTravis = async (gh) => {
+  const url = `https://travis-ci.org/${gh}.svg?branch=master`
+
+  if (await badgeExists(url)) {
+    return `[![Travis CI](${url})](https://travis-ci.org/${gh})`
+  } else {
+    return 'N/A'
+  }   
+}
+
+const ciJenkins = async (gh) => {
+  // Need to fix the path for jenkins links, as jenkins adds `/job/` between everything
+  const jenkinsPath = gh.split('/').join('/job/')
+  const badge = `https://ci.ipfs.team/buildStatus/icon?job=${gh}/master`
+  
+  if (await badgeExists(badge)) {
+    return `[![jenkins](${badge})](https://ci.ipfs.team/job/${jenkinsPath}/job/master/)`
+  } else {
+    return 'N/A'
+  } 
+}
+
+const coverage = (gh) => {
+  return `[![codecov](https://codecov.io/gh/${gh}/branch/master/graph/badge.svg)](https://codecov.io/gh/${gh})`
+}
+
+const description = (gh, pkg, desc) => desc
+
+module.exports = {
+  Package: name,
+  Name: name,
+  Version: npmVersion,
+  Deps: deps,
+  CI: ciJenkins,
+  'CI/Jenkins': ciJenkins,
+  'CI/Travis': ciTravis,
+  Coverage: coverage,
+  Description: description
+}
--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@
 
 const yargs = require('yargs')
 const path = require('path')
+const packageBadges = require('./badges')
 const argv = yargs.argv
 
 if (!argv.data) {
@@ -12,51 +13,28 @@ if (!argv.data) {
 
 const data = require(path.join(process.cwd(), argv.data))
 
-const isItemPackage = (item) => Array.isArray(item)
-
-const packageBadges = [
-  // Package
-  (gh, npm) => `[\`${npm}\`](//github.com/${gh})`,
-  // Version
-  (gh, npm) => `[![npm](https://img.shields.io/npm/v/${npm}.svg?maxAge=86400&style=flat-square)](//github.com/${gh}/releases)`,
-  // Deps
-  (gh, npm) => `[![Deps](https://david-dm.org/${gh}.svg?style=flat-square)](https://david-dm.org/${gh})`,
-  // CI
-  (gh, npm) => {
-    // Need to fix the path for jenkins links, as jenkins adds `/job/` between everything
-    const jenkinsPath = gh.split('/').join('/job/')
-    return `[![jenkins](https://ci.ipfs.team/buildStatus/icon?job=${gh}/master)](https://ci.ipfs.team/job/${jenkinsPath}/job/master/)`
-  },
-  // Coverage
-  (gh, npm) => `[![codecov](https://codecov.io/gh/${gh}/branch/master/graph/badge.svg)](https://codecov.io/gh/${gh})`
-]
-
 // Creates the table row for a package
-const generatePackageRow = (item) => {
-  const row = packageBadges.map((func) => {
-    // First string is GitHub path, second is npm package name
-    return func(item[0], item[1])
-  }).join(' | ')
-  const fullRow = `| ${row} |`
-  return fullRow
+const generatePackageRow = async item => {
+  const promises = data.columns.map(col => packageBadges[col](...item))
+  const vals = await Promise.all(promises)
+
+  return `| ${vals.join(' | ')} |`
 }
 
 // Generates a row for the table, depending if it's a package or a heading
-const generateRow = (item) => {
-  if (isItemPackage(item)) {
+const generateRow = item => {
+  if (Array.isArray(item)) {
     return generatePackageRow(item)
   } else {
     return `| **${item}** |`
   }
 }
 
-const header = `| ${data.columns.join(' | ')} |`
-const hr = `| ${data.columns.map(() => '---------').join('|')} |`
+(async () => {
+  const header = `| ${data.columns.join(' | ')} |`
+  const hr = `| ${data.columns.map(() => '---------').join('|')} |`
+  const promises = data.rows.map(row => generateRow(row))
+  const rows = (await Promise.all(promises)).join('\n')
 
-const toPrint = [
-  header,
-  hr,
-  data.rows.map((row) => generateRow(row)).join('\n')
-]
-
-toPrint.forEach((t) => console.log(t))
+  console.log([header, hr, rows].join('\n'))
+})()
